38a567d5f63c495252222896737245e9743ea47d
mauricioACV <mauricio.campusano@gmail.com>
2021-06-29 21:19:50 -0400

    Cambios generales en diseño de la página. seccion "nosotros" y "servicio" en un 90% implementadas. Efecto sobre cada card en "servicios" implementado con solo CSS. Próximos avances: slider de fotos en portafolio de trabajos de demarcaciones integrando glider.js

--- js/app.js
+++ js/app.js
@@ -1,15 +1,27 @@
 const typed = new Typed('.typed', {
-    strings: ['Demarcaciones Viales <br><i>Calidad asegurada...</i>'],
+    strings: ['Seguridad Industrial y Demarcaciones Viales <br><i>Compromiso y calidad asegurada...</i>'],
 	stringsElement: '#cadenas-texto', // ID del elemento que contiene cadenas de texto a mostrar.
-	typeSpeed: 100, // Velocidad en mlisegundos para poner una letra,
+	typeSpeed: 50, // Velocidad en mlisegundos para poner una letra,
 	startDelay: 500, // Tiempo de retraso en iniciar la animacion. Aplica tambien cuando termina y vuelve a iniciar,
 	backSpeed: 75, // Velocidad en milisegundos para borrrar una letra,
 	smartBackspace: true, // Eliminar solamente las palabras que sean nuevas en una cadena de texto.
 	shuffle: false, // Alterar el orden en el que escribe las palabras.
-	backDelay: 15000, // Tiempo de espera despues de que termina de escribir una palabra.
+	backDelay: 30000, // Tiempo de espera despues de que termina de escribir una palabra.
 	loop: true, // Repetir el array de strings
 	loopCount: false, // Cantidad de veces a repetir el array.  false = infinite
 	showCursor: true, // Mostrar cursor palpitanto
 	cursorChar: '_', // Caracter para el cursor
 	contentType: 'html', // 'html' o 'null' para texto sin formato
 });
+
+
+const LottieContainer = document.querySelector('.LottieContainer')
+
+var lottieAnimation = bodymovin.loadAnimation({
+	container: LottieContainer, // ID del div
+	path: './svg/watermelon-pack-animation-03.json', // Ruta fichero .json de la animación
+	renderer: 'svg', // Requerido
+	loop: true, // Opcional
+	autoplay: true, // Opcional
+	name: "Hello World", // Opcional
+  })
